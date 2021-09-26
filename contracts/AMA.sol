a108224ba11cb39a67545d615de209cedb07b547
Martin Bavio <mbavio@gmail.com>
2021-09-26 10:44:20 -0300

    Add basic functions to the contract and run those on the script

--- contracts/AMA.sol
+++ contracts/AMA.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract AMA {
+    uint256 questionsCount;
+
     constructor() {
         console.log("I'm alive!");
+    }
+
+    function ask() public {
+        questionsCount += 1;
+        console.log("%s has asked a question", msg.sender);
+    }
+
+    function getTotalQuestions() public view returns (uint256) {
+        console.log("Total questions: %s", questionsCount);
+        return questionsCount;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -3,10 +3,25 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [owner, ...accounts] = await hre.ethers.getSigners();
   const AMAFactory = await hre.ethers.getContractFactory("AMA");
   const AMA = await AMAFactory.deploy();
   await AMA.deployed();
+
   console.log("Contract deployed to:", AMA.address);
+  console.log("Constract deployed by:", owner.address);
+
+  await AMA.getTotalQuestions();
+
+  const txn = await AMA.ask();
+  await txn.wait();
+
+  await AMA.getTotalQuestions();
+
+  const anotherTxn = await AMA.connect(accounts[0]).ask();
+  await anotherTxn.wait();
+
+  await AMA.getTotalQuestions();
 }
 
 main()
