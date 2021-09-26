3f7d898f0a331584d3e2833b66b9612a93c28ab7
Martin Bavio <mbavio@gmail.com>
2021-09-26 10:29:44 -0300

    Add script to run the contract on hardhat

---
+++ scripts/run.js
@@ -0,0 +1,19 @@
+// Require the Hardhat Runtime Environment explicitly. Optional
+// but useful for running the script through `node <script>`.
+const hre = require("hardhat");
+
+async function main() {
+  const AMAFactory = await hre.ethers.getContractFactory("AMA");
+  const AMA = await AMAFactory.deploy();
+  await AMA.deployed();
+  console.log("Contract deployed to:", AMA.address);
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
