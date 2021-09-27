2f8f5dd2d150e90c6c72f9f1a555d6fe5fdf3622
Martin Bavio <mbavio@gmail.com>
2021-09-27 10:40:06 -0300

    Show questions on the UI

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import { ethers } from 'ethers';
 import * as React from 'react';
+
+import contractABI from '../artifacts/contracts/AMA.sol/AMA.json';
 
 const METAMASK_ACTIONS = {
   eth_accounts: 'eth_accounts',
@@ -6,32 +9,10 @@ const METAMASK_ACTIONS = {
 };
 
 export default function Index() {
+  const { questions, ask } = useQuestions();
   const { account, connect } = useEthAccount();
+  const [question, setQuestion] = React.useState('');
 
-  React.useEffect(() => {
-    const { ethereum } = window;
-    if (!ethereum) {
-      console.log('No access to the Ethereum object');
-      return;
-    }
-
-    const checkAccounts = async () => {
-      try {
-        const accounts = await ethereum.request({
-          method: METAMASK_ACTIONS.eth_accounts,
-        });
-        if (accounts.length > 0) {
-          setAccount(accounts[0]);
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    checkAccounts();
-  }, []);
-
-  console.log('ACCOUNT IS', account);
   return (
     <div>
       Welcome to my AMA{' '}
@@ -39,6 +20,25 @@ export default function Index() {
         <button type="button" onClick={connect}>
           Connect Wallet
         </button>
+      )}
+      {account && (
+        <>
+          <input
+            type="text"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+          <button type="button" onClick={() => ask(question)}>
+            Ask a question
+          </button>
+        </>
+      )}
+      {questions.length > 0 && (
+        <ul>
+          {questions.map((q) => (
+            <li key={q.createdAt}>{q.question}</li>
+          ))}
+        </ul>
       )}
     </div>
   );
@@ -92,4 +92,58 @@ function useEthAccount() {
   }
 
   return { account, connect };
+}
+
+function useQuestions() {
+  const { account } = useEthAccount();
+  const [questions, setQuestions] = React.useState([]);
+
+  const getQuestions = React.useCallback(async () => {
+    if (!account) {
+      console.log('No wallet connected');
+      return;
+    }
+
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+      contractABI.abi,
+      signer,
+    );
+
+    const rawQuestions = await contract.getAllQuestions();
+    setQuestions(
+      rawQuestions.map((q) => ({
+        creator: q.creator,
+        question: q.question,
+        createdAt: new Date(q.timestamp.toNumber() * 1000),
+      })),
+    );
+  }, [account]);
+
+  async function ask(question) {
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+      contractABI.abi,
+      signer,
+    );
+
+    const txn = await contract.ask(question);
+    console.log('Mining...', txn.hash);
+
+    await txn.wait();
+    console.log('Mined -- ', txn.hash);
+    getQuestions();
+  }
+
+  React.useEffect(() => {
+    getQuestions();
+  }, [account, getQuestions]);
+
+  return { questions, ask };
 }
