67d8df2bcfeba9031cabe9ef4b2bc592a785f931
Martin Bavio <mbavio@gmail.com>
2021-09-27 11:21:31 -0300

    Add cooldown to the ask function

--- contracts/AMA.sol
+++ contracts/AMA.sol
@@ -18,16 +18,25 @@ contract AMA {
 
   Question[] questions;
 
+  mapping(address => uint256) public lastQuestionTimestamp;
+
   constructor() payable {
     console.log("I'm alive!");
   }
 
   function ask(string memory _question) public {
-    questionsCount += 1;
-    console.log("%s has asked a question", msg.sender);
+    require(
+      lastQuestionTimestamp[msg.sender] + 1 days <= block.timestamp,
+      'You can ask only once per day'
+    );
+
+    lastQuestionTimestamp[msg.sender] = block.timestamp;
 
     uint256 random = (block.difficulty + block.timestamp + salt) % 100;
     salt = random;
+
+    questionsCount += 1;
+    console.log("%s has asked a question", msg.sender);
 
     questions.push(Question(msg.sender, _question, block.timestamp));
     emit NewQuestion(msg.sender, block.timestamp, _question);
